11ce4bb38335bf8f00f53237aa72ae08bb0a69e2
jose <josecarlosrqy@gmail.com>
2026-03-09 22:53:45 +0100

    Página de genre. Responses de genres

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import CategoryPage from './pages/CategoryPage'
+import GenrePage from './pages/GenrePage'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/categoria/:id" element={<CategoryPage />} />
+      <Route path="/genre/:id" element={<GenrePage />} />
     </Routes>
   )
 }
--- src/pages/GenrePage.tsx
+++ src/pages/GenrePage.tsx
@@ -1,11 +1,11 @@
 import { useParams } from "react-router-dom"
 
-export default function CategoryPage() {
+export default function GenrePage() {
   const { id } = useParams()
 
   return (
 	<main className="min-h-screen text-white">
-	  <h1>Category Page</h1>
+	  <h1>Genre Page</h1>
 	  <p>Category ID: {id}</p>
 	</main>
   )
